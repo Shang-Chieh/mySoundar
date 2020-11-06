a08cdbad90a66e7c30424ad462e9efb66fd03afa
chieh5350 <chieh5350@gmail.com>
2020-11-06 23:31:19 +0800

    StudioMain照片切換

--- reactproj/src/ch_components/MyNavbar.js
+++ reactproj/src/ch_components/MyNavbar.js
@@ -5,8 +5,6 @@ import {
 } from 'react-bootstrap'
 
 import logofordark from '../ch_svg/logofordark.svg'
-
-//選單連結要使用NavLink來取代Link,這個元件是特別針對像選單項目這種導覽所設計的,會有active點亮的效果(被點按到時的特定css)
 import { NavLink } from 'react-router-dom'
 
 function MyNavbar(props) {
@@ -24,10 +22,6 @@ function MyNavbar(props) {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            {/* 把Nav.Link作為NavLink來使用 */}
-            {/* <Nav.Link as={NavLink} to="/activitylist" >
-              活動列表
-            </Nav.Link> */}
             <Nav.Link as={NavLink} to="/activitymain">
               活動
             </Nav.Link>
@@ -37,9 +31,6 @@ function MyNavbar(props) {
             <Nav.Link as={NavLink} to="/activityorder">
               活動訂單查詢
             </Nav.Link>
-            {/* <Nav.Link as={NavLink} to="/studiolist" >
-              場地列表
-            </Nav.Link> */}
             <Nav.Link as={NavLink} to="/studiomain">
               場地
             </Nav.Link>
--- reactproj/src/ch_pages/StudioMain.js
+++ reactproj/src/ch_pages/StudioMain.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 import { withRouter } from 'react-router-dom'
 import Rater from 'react-rater'
 import 'react-rater/lib/react-rater.css'
@@ -11,10 +13,52 @@ import { Button, Tabs, Tab } from 'react-bootstrap';
 
 
 function StudioMain(props){
+  //圖片切換
+  class MyCarousel extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        value: 0,
+        slides: [
+          (<img src={"http://localhost:3000/ch_img/s000101.jpg"} className="img-demo"/>),
+          (<img src={"http://localhost:3000/ch_img/s000102.jpg"} className="img-demo"/>),
+          (<img src={"http://localhost:3000/ch_img/s000103.jpg"} className="img-demo"/>),
+        ],
+        thumbnails: [
+          (<img src={"http://localhost:3000/ch_img/s000101.jpg"} className="studio-head-img w-100"/>),
+          (<img src={"http://localhost:3000/ch_img/s000102.jpg"} className="studio-head-img w-100"/>),
+          (<img src={"http://localhost:3000/ch_img/s000103.jpg"} className="studio-head-img w-100"/>),
+        ],
+      }
+      this.onchange = this.onchange.bind(this);
+    }
+  
+  
+    onchange(value) {
+      this.setState({ value });
+    }
+  
+    render() {
+      return (
+      <div className="img-area">
+        <Carousel
+          value={this.state.value}
+          slides={this.state.slides}
+          onChange={this.onchange}
+        />
+        <div className="img-wrap d-flex">
+          <span><MdKeyboardArrowLeft /></span>
+          <Dots number={this.state.thumbnails.length} thumbnails={this.state.thumbnails} value={this.state.value} onChange={this.onchange} number={this.state.slides.length}/>
+          <span><MdKeyboardArrowRight /></span> 
+        </div>
+      </div>
+      );
+    }
+  }
 
+    //方案,介紹,評價分頁切換
     function ControlledTabs() {
-        const [key, setKey] = useState('option');
-      
+        const [key, setKey] = useState('option');      
         return (
           <Tabs
             id="controlled-tab-studio"
@@ -38,8 +82,26 @@ function StudioMain(props){
     return(
         <>
             <Breadcrumb />
-                <div className="main d-flex">
-                    <div className="img-area">
+            <div className="main d-flex">
+            <MyCarousel />
+              <div className="studio-introduction">
+                  <h2>LAZI Corner</h2>
+                  <h3 className="text-success">NT$ 500</h3>
+                  <span>顧客評價
+                  <Rater rating={4.8} total={5} interactive={false}/>
+                  4.8(25)</span>
+                  <p><FaMapMarkerAlt />地點：台北市大安區復興南路一段390號</p>
+                  <Button variant="secondary" className="btn-lg" onClick={()=>{props.history.push('/studiomain')}}>選擇方案</Button>
+              </div>
+            </div>
+            <ControlledTabs/>   
+        </>
+    )
+}
+
+export default withRouter(StudioMain)
+
+                    {/* <div className="img-area">
                          <img src={"http://localhost:3000/ch_img/s000101.jpg"} className="img-demo"/>   
                          <div className="img-wrap d-flex justify-content-between">
                             <MdKeyboardArrowLeft />
@@ -48,21 +110,4 @@ function StudioMain(props){
                             <img src={"http://localhost:3000/ch_img/s000103.jpg"} className="studio-head-img"/> 
                             <MdKeyboardArrowRight />  
                          </div>
-                    </div>
-                    <div className="studio-introduction">
-                        <h2>LAZI Corner</h2>
-                        <h3 className="text-success">NT$ 500</h3>
-                        <span>顧客評價
-                        <Rater rating={4.8} total={5} interactive={false}/>
-                        4.8(25)</span>
-                        <p><FaMapMarkerAlt />地點：台北市大安區復興南路一段390號</p>
-                        <Button variant="secondary" className="btn-lg" onClick={()=>{props.history.push('/studiomain')}}>選擇方案</Button>
-                    </div>
-                </div>
-
-                <ControlledTabs/>   
-        </>
-    )
-}
-
-export default withRouter(StudioMain)
+                    </div> */}
