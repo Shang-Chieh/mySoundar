bbb585a5a7a32713dc99b680b54c64c81383f229
Shang-Chieh <sky800227@gmail.com>
2020-11-10 15:35:36 +0800

    報名資訊Modal

--- reactproj/src/chieh/ch_components/ActivityOption.js
+++ reactproj/src/chieh/ch_components/ActivityOption.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { MdAddCircleOutline,MdRemoveCircleOutline } from 'react-icons/md'
-import { Button, Accordion, Card } from 'react-bootstrap'
+import { Button, Accordion, Card, Modal, Form, Row, Col } from 'react-bootstrap'
 
 function ActivityOption(props) {
   const [quantity, setQuantity] = useState(1)
+  const [total, setTotal] = useState(0)
+
+  //立即報名Modal  
+  const [show, setShow] = useState(false);  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);  
 
   return (
     <>
@@ -53,10 +59,97 @@ function ActivityOption(props) {
                       <hr/>
                       <div className="d-flex align-items-center justify-content-end mb-3">
                         <div className="mr-4">總金額</div>
-                          <div style={{color:'#2690df', fontWeight:'bold', fontSize:'1.5rem', fontFamily:'Roboto'}}>NT$ {`${quantity}`*`${option.slice(4,9)}`}</div>
+                          <div style={{color:'#2690df', fontWeight:'bold', fontSize:'1.5rem', fontFamily:'Roboto'}}>
+                            NT$ {`${quantity}`*`${option.slice(4,9)}`}</div>
                         </div>
                         <div className="d-flex justify-content-end">
-                          <button type="button" className="btn btn-option ml-auto" onClick={()=>{props.history.push('/activitycart')}}>立即購票</button>
+                        <button type="button" className="btn btn-option" onClick={handleShow}>立即報名</button>
+                          <Modal
+                            show={show}
+                            onHide={handleClose}
+                            backdrop="static"
+                            keyboard={false}
+                            size="lg"
+                            aria-labelledby="contained-modal-title-vcenter"
+                            centered
+                          >
+                            <Modal.Header closeButton>
+                              <Modal.Title>確認報名項目</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form key={value}>
+                              <Form.Group as={Row} controlId="formPlaintextEmail">
+                                <Form.Label column sm="2">活動名稱</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue={value.activity_name} />
+                                </Col>
+                              </Form.Group>
+
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">活動日期</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue={value.activity_date} />
+                                </Col>
+                              </Form.Group> 
+
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">方案</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue="早鳥票" />
+                                </Col>
+                              </Form.Group>
+
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">費用</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue="$4000" />
+                                </Col>
+                              </Form.Group> 
+
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">數量</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue={quantity} />
+                                </Col>
+                              </Form.Group>
+
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">小計</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue="$4000" />
+                                </Col>
+                              </Form.Group>  
+                              <hr />
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">報名者姓名</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue="michael" />
+                                </Col>
+                              </Form.Group> 
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">報名者手機</Form.Label>
+                                <Col sm="10">
+                                  <Form.Control plaintext readOnly defaultValue="0977777555" />
+                                </Col>
+                              </Form.Group>
+                              <Form.Group as={Row} controlId="formPlaintextPassword">
+                                <Form.Label column sm="2">報名者E-mail</Form.Label>
+                                <Col sm="4">
+                                  <Form.Control plaintext readOnly defaultValue="michael.sps168@gmail.com" />
+                                </Col>
+                                <Col sm="6">
+                                  <Form.Text className="text-muted">電子票券將寄至此信箱</Form.Text>
+                                </Col>                                
+                              </Form.Group>                         
+                              </Form>                             
+                            </Modal.Body>
+                            <Modal.Footer>
+                              <Button variant="secondary" onClick={handleClose}>
+                                取消
+                              </Button>
+                              <Button variant="primary" type="submit" onClick={handleClose}>完成報名</Button>
+                            </Modal.Footer>
+                          </Modal>
                         </div>
                       </div>
                       </Card.Body>
@@ -67,7 +160,6 @@ function ActivityOption(props) {
          </div>
            )
          })}
-
     </>
   )
 }
